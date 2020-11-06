e5d4f2703df9157fa84dddf189ea4944f6c4f632
dularion <antonia.hulha@systaro.de>
2020-11-06 13:11:31 +0100

    further simplify error

--- src/main/groovy/basex/BasexAssociation.groovy
+++ src/main/groovy/basex/BasexAssociation.groovy
@@ -1,10 +1,5 @@
 package basex
 
-/**
- * Abstract class for all associations which can be used in the basexHasMany
- * @param <T>
- */
-
 abstract class BasexAssociation {
   protected String name
 
--- src/main/groovy/basex/BasexEntityReference.groovy
+++ src/main/groovy/basex/BasexEntityReference.groovy
@@ -1,20 +1,11 @@
 package basex
 
-/**
- * Creates an association between two Domains, which are reference by an entityClass and entityId on one side of the association.
- * The enclosedClass holds the properties entityId/entityClass
- * The referencedClass is referenced in these properties
- *
- * example usage:  static basexHasMany = [textBlocks: new BasexEntityReference<TextBlock>(TextBlock, Product)]
- *
- * @param < T >
- */
 
 class BasexEntityReference extends BasexAssociation {
   String status
 
   BasexEntityReference(String name, String status) {
-    this.name = name
+    this.name = name  //this will break the application, because it sets the property of the abstract superclass
     this.status = status
   }
 }
